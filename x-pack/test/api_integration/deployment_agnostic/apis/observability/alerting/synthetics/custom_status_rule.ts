41a468ceb1eca80b421b6109dc2b87be74722305
Francesco Fagnani <fagnani.francesco@gmail.com>
2025-05-30 17:50:23 +0200

    [Synthetics] Skipping synthetics alerting tests on cloud (#222058)
    (cherry picked from commit c03bcbcaa27d61f139655748845f3b1acac14906)
    
    # Conflicts:
    #	x-pack/test/api_integration/deployment_agnostic/apis/observability/alerting/synthetics/custom_status_rule.ts

--- x-pack/test/api_integration/deployment_agnostic/apis/observability/alerting/synthetics/custom_status_rule.ts
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/alerting/synthetics/custom_status_rule.ts
@@ -30,8 +30,10 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
   let adminRoleAuthc: RoleCredentials;
   const samlAuth = getService('samlAuth');
 
-  // Failing: See https://github.com/elastic/kibana/issues/202337
-  describe.skip('SyntheticsCustomStatusRule', () => {
+  describe('SyntheticsCustomStatusRule', function () {
+    // Test failing on MKI and ECH
+    this.tags(['skipCloud']);
+
     const SYNTHETICS_RULE_ALERT_INDEX = '.alerts-observability.uptime.alerts-default';
 
     before(async () => {
